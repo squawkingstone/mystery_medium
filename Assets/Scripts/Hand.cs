cec9e9bbfc0b5ddb94b1492276f13ee0cb750601
Wright <wrighl@win.rpi.edu>
2018-11-12 18:19:50 -0500

    Test VR stuff

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Hand : MonoBehaviour 
+{	
+	[SerializeField] bool left;
+
+	SteamVR_Input_Sources input;
+
+	void Awake()
+	{
+		input = (left) ? SteamVR_Input_Sources.LeftHand : SteamVR_Input_Sources.RightHand;
+	}
+
+	void Update()
+	{
+		if (SteamVR_Input._default.inActions.Teleport.GetStateDown(input) 
+			&& input == SteamVR_Input_Sources.LeftHand)
+			Debug.Log("TELEPORT");
+		// here, check if I'm intersecting a thing, and then if get state down for the trigger
+		// parent the object
+	}
+}
