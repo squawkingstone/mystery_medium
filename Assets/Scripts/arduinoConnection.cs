f453c1d13c03c1017aca6871b74ff0331e9e3613
figgyfunk <figa@rpi.edu>
2018-11-28 14:45:51 -0500

    moved in arduino stuff.

---
+++ Assets/Scripts/arduinoConnection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+
+public class arduinoConnection : MonoBehaviour
+{
+
+    SerialPort stream = new SerialPort("COM3", 9600);
+    int button = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stream.Open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string value = stream.ReadLine();
+        button = int.Parse(value);
+        Debug.Log(button);
+    }
+}
