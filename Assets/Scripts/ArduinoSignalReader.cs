3c28f2510adc8e78e7801cbbf3cc850792dcc2f3
Lorelei Wright <wrighl@rpi.edu>
2018-12-09 17:36:49 -0500

    Toggling and arduino stuff

---
+++ Assets/Scripts/ArduinoSignalReader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoSignalReader : MonoBehaviour {
+
+	public static SerialPort port = new SerialPort("COM4", 9600, Parity.None, 8, StopBits.One);      
+     
+    void Start () 
+    {
+    	OpenDeviceConnection();
+    }
+ 
+    void Update()
+    {
+     	Debug.Log(port.ReadLine());
+    }
+     
+    void OpenDeviceConnection()
+	{
+		port.Open();
+	}
+ 
+    void OnApplicationQuit() 
+    {
+        port.Close();
+    }
+}
---
+++ Assets/Scripts/ToggleProp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleProp : MonoBehaviour 
+{
+	/* I'll figure out how to do it so this works with single objects (toggles
+	 * them on and off), and switching objects.
+	 */
+
+	GameObject obj1 = null;
+	GameObject obj2 = null;
+
+	void Start () 
+	{
+		Debug.Log(name + " : " + transform.childCount);
+		if (transform.childCount == 2)
+		{
+			obj1 = transform.GetChild(0).gameObject;
+			obj2 = transform.GetChild(1).gameObject;
+			obj1.SetActive(true);
+			obj2.SetActive(false);
+		}
+		else
+		{
+			obj1 = transform.GetChild(0).gameObject;
+			obj1.SetActive(false);
+		}
+	}
+
+	[ContextMenu("Toggle")]
+	public void Toggle()
+	{
+		bool t = obj1.activeInHierarchy;
+		obj1.SetActive(!t);
+		if (obj2 != null) obj2.SetActive(t);
+	}
+}
